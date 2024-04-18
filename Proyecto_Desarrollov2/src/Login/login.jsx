4c6b4ba2b4bd80046294f8691b31e66a0c8ff9ed
lfhernandez03 <Luishsg1@gmail.com>
2024-04-18 13:22:19 -0500

    Update: React y Express.js

---
+++ Proyecto_Desarrollov2/src/Login/login.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import './login.css'
+
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.Fragment>
+    <h1>Hola mundo</h1>
+  </React.Fragment>
+)
