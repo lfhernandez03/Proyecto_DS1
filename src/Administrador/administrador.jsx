61ebc9253676d47a7f59b2fb39735ddb4eb266ae
Dieguit0000 <diegofernandomorenomartinez@gmail.com>
2024-04-28 15:00:36 -0500

    Interface Administrador
    Interface con funcionalidades del administrador

--- src/Administrador/administrador.jsx
+++ src/Administrador/administrador.jsx
@@ -1,81 +1,267 @@
 import React from "react";
-import { ButtonAdmin, ButtonLink, BasicMenu } from "./administradorComponents";
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import {
+  ButtonAdmin,
+  TextFieldsAdmin,
+  BasicMenu,
+  BasicSelect,
+  BasicSelectTipo,
+} from "./administradorComponents";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import { AdminLayout } from "./AdministradorLayout";
 
 export const Admin = () => {
-    return (
-        <div className="cuerpo">
-            <div>
-                <h1>Bienvenido,</h1>
-                <h3>Usuario</h3>
-                <h4 className="ques">¿Qué quieres hacer hoy?</h4>
-                <div className="menu">
-                    <BasicMenu name="Reservas" option1="Crear Reserva" option2="Buscar Reserva" link1="/CrearReserva" link2="/BuscarReserva" />
-                    <BasicMenu name="Empleados" option1="Crear Empleados" option2="Buscar Empleados" link1="/CrearEmpleado" link2="/BuscarEmpleado" />
-                    <BasicMenu name="Habitaciones" option1="Crear Habitación" option2="Buscar Habitación" link1="/CrearHabitación" link2="/BuscarHabitación" />
-                </div>
-            </div>
+  return (
+    <div className="cuerpo">
+      <div>
+        <h1>Bienvenido,</h1>
+        <h3>Usuario</h3>
+        <h4 className="ques">¿Qué quieres hacer hoy?</h4>
+        <div className="menu">
+          <BasicMenu
+            name="Reservas"
+            option1="Crear Reserva"
+            option2="Buscar Reserva"
+            link1="/CrearReserva"
+            link2="/BuscarReserva"
+          />
+          <BasicMenu
+            name="Empleados"
+            option1="Crear Empleados"
+            option2="Buscar Empleados"
+            link1="/CrearEmpleado"
+            link2="/BuscarEmpleado"
+          />
+          <BasicMenu
+            name="Habitaciones"
+            option1="Crear Habitación"
+            option2="Buscar Habitación"
+            link1="/CrearHabitación"
+            link2="/BuscarHabitación"
+          />
         </div>
-    );
+      </div>
+    </div>
+  );
 };
 
 export const ContainerCrearReserva = () => {
-    return (
+  return (
     <>
-    <AdminLayout>
-        <h1>Formulario de Reserva</h1>
-    </AdminLayout>
+      <AdminLayout>
+        <div className="forms" id="1">
+          <h1>Formulario de Reserva</h1>
+          <h3>Informacion del cliente</h3>
+          <div className="cuerpo-form">
+            <TextFieldsAdmin label="Nombre" name="name" type="name" />
+            <TextFieldsAdmin label="Identificación" name="id" type="id" />
+            <TextFieldsAdmin
+              label="Correo electrónico"
+              name="email"
+              type="email"
+            />
+            <TextFieldsAdmin label="Telefono" name="tel" type="number" />
+          </div>
+          <h3>Informacion de la reserva</h3>
+          <div className="cuerpo-form">
+            <TextFieldsAdmin
+              label="Fecha entrada"
+              name="Fecha-in"
+              type="date"
+            />
+            <TextFieldsAdmin
+              label="Fecha salida"
+              name="Fecha-out"
+              type="date"
+            />
+            <BasicSelect />
+            <TextFieldsAdmin label="ID" name="id" type="number" />
+          </div>
+          <div>
+            <ButtonAdmin type="submit" value="Reservar" label="Hacer reserva" />
+          </div>
+        </div>
+      </AdminLayout>
     </>
-    );
+  );
 };
 
 export const ContainerBuscarReserva = () => {
-    return (
+  return (
     <>
-    <AdminLayout>
-        <h1>Busqueda de Reserva</h1>
-    </AdminLayout>
+      <AdminLayout>
+        <div className="forms" id="2">
+          <h1>Busqueda de Reserva</h1>
+          <h3>Informacion del cliente</h3>
+          <div className="cuerpo-form">
+            <TextFieldsAdmin label="Identificación" name="id" type="number" />
+          </div>
+          <h3>Informacion de la reserva</h3>
+          <div className="cuerpo-form">
+            <TextFieldsAdmin
+              label="Fecha entrada"
+              name="Fecha-in"
+              type="date"
+            />
+            <TextFieldsAdmin
+              label="Fecha salida"
+              name="Fecha-out"
+              type="date"
+            />
+            <BasicSelect />
+            <TextFieldsAdmin label="ID" name="id" type="id" />
+          </div>
+          <div className="botones">
+            <ButtonAdmin
+              type="submit"
+              value="Eliminar-reserva"
+              label="Eliminar reserva"
+            />
+            <ButtonAdmin
+              type="submit"
+              value="Modificar-reserva"
+              label="Modificar reserva"
+            />
+          </div>
+        </div>
+      </AdminLayout>
     </>
-    );
-}
+  );
+};
 
 export const ContainerCrearEmpleado = () => {
-    return (
+  return (
     <>
-    <AdminLayout>
-        <h1>Formulario de Empleado</h1>
-    </AdminLayout>
+      <AdminLayout>
+        <div className="forms" id="1">
+          <h1>Formulario de Empleado</h1>
+          <h3>Informacion del empleado</h3>
+          <div className="cuerpo-form">
+            <TextFieldsAdmin label="Nombre" name="name" type="name" />
+            <TextFieldsAdmin label="Identificación" name="id" type="id" />
+            <TextFieldsAdmin
+              label="Correo electrónico"
+              name="email"
+              type="email"
+            />
+            <TextFieldsAdmin label="Telefono" name="tel" type="number" />
+          </div>
+          <div className="cuerpo-form">
+            <TextFieldsAdmin
+              label="Fecha inicio"
+              name="Fecha-begin"
+              type="date"
+            />
+            <TextFieldsAdmin
+              label="Fecha nacimiento"
+              name="Fecha-nacimiento"
+              type="date"
+            />
+            <TextFieldsAdmin label="Salario" name="salario" type="number" />
+            <TextFieldsAdmin label="Contraseña" name="id" type="password" />
+            <BasicSelectTipo />
+          </div>
+          <div>
+            <ButtonAdmin
+              type="submit"
+              value="crear-empleado"
+              label="Crear Empleado"
+            />
+          </div>
+        </div>
+      </AdminLayout>
     </>
-    );
+  );
 };
 
 export const ContainerBuscarEmpleado = () => {
-    return (
+  return (
     <>
-    <AdminLayout>
-        <h1>Busqueda de Empleado</h1>
-    </AdminLayout>
+      <AdminLayout>
+        <div className="forms" id="2">
+          <h1>Busqueda de Empleado</h1>
+          <h3>Información del Empleado</h3>
+          <div className="cuerpo-form">
+            <TextFieldsAdmin label="Identificación" name="id" type="number" />
+          </div>
+          <div className="cuerpo-form">
+            <TextFieldsAdmin label="Nombre empleado" name="name" type="name" />
+            <TextFieldsAdmin label="Correo" name="correo" type="email" />
+            <BasicSelectTipo />
+            <TextFieldsAdmin label="Contraseña" name="id" type="password" />
+          </div>
+          <div className="botones">
+            <ButtonAdmin
+              type="submit"
+              value="Eliminar-empleado"
+              label="Eliminar"
+            />
+            <ButtonAdmin
+              type="submit"
+              value="Modificar-empleado"
+              label="Modificar"
+            />
+          </div>
+        </div>
+      </AdminLayout>
     </>
-    );
-}
+  );
+};
 
 export const ContainerCrearHabitación = () => {
-    return (
+  return (
     <>
-    <AdminLayout>
-        <h1>Formulario de Habitación</h1>
-    </AdminLayout>
+      <AdminLayout>
+        <div className="forms" id="2">
+          <h1>Formulario de Habitacion</h1>
+          <h3>Información de la Habitación</h3>
+          <div className="cuerpo-form">
+            <TextFieldsAdmin label="Tipo Habitación" name="type" type="text" />
+            <TextFieldsAdmin label="Id" name="id" type="number" />
+            <TextFieldsAdmin label="Capacidad" name="cap" type="number" />
+            <TextFieldsAdmin label="Precio" name="price" type="number" />
+            <BasicSelect />
+          </div>
+          <ButtonAdmin
+            type="submit"
+            value="crear-habitacion"
+            label="Crear Habitación"
+          />
+        </div>
+      </AdminLayout>
     </>
-    );
+  );
 };
 
 export const ContainerBuscarHabitación = () => {
-    return (
+  return (
     <>
-    <AdminLayout>
-        <h1>Busqueda de Habitación</h1>
-    </AdminLayout>
+      <AdminLayout>
+        <div className="forms" id="2">
+          <h1>Busqueda de Habitación</h1>
+          <h3>Información de la habitación</h3>
+          <div className="cuerpo-form">
+            <TextFieldsAdmin label="Identificación" name="id" type="number" />
+          </div>
+          <div className="cuerpo-form">
+            <TextFieldsAdmin label="Tipo" name="type" type="type" />
+            <TextFieldsAdmin label="Capacidad" name="cap" type="number" />
+            <TextFieldsAdmin label="Precio" name="price" type="number" />
+            <BasicSelect />
+          </div>
+          <div className="botones">
+            <ButtonAdmin
+              type="submit"
+              value="Eliminar-habitacion"
+              label="Eliminar"
+            />
+            <ButtonAdmin
+              type="submit"
+              value="Modificar-habitacion"
+              label="Modificar"
+            />
+          </div>
+        </div>
+      </AdminLayout>
     </>
-    );
-}
+  );
+};
--- src/Administrador/administradorComponents.jsx
+++ src/Administrador/administradorComponents.jsx
@@ -1,74 +1,135 @@
-import React from 'react';
-import Button from '@mui/material/Button';
-import MenuItem from '@mui/material/MenuItem'; // Corregir la importación de MenuItem
-import { Link } from 'react-router-dom';
-import Menu from '@mui/material/Menu';
+import React from "react";
+import Button from "@mui/material/Button";
+import MenuItem from "@mui/material/MenuItem"; // Corregir la importación de MenuItem
+import { Link } from "react-router-dom";
+import Menu from "@mui/material/Menu";
+import TextField from "@mui/material/TextField";
+import Box from "@mui/material/Box";
+import InputLabel from "@mui/material/InputLabel";
+import FormControl from "@mui/material/FormControl";
+import Select from "@mui/material/Select";
 
 export function BasicMenu(props) {
-    const [anchorEl, setAnchorEl] = React.useState(null);
-    const open = Boolean(anchorEl);
-    const handleClick = (event) => {
-        setAnchorEl(event.currentTarget);
-    };
-    const handleClose = () => {
-        setAnchorEl(null);
-    };
+  const [anchorEl, setAnchorEl] = React.useState(null);
+  const open = Boolean(anchorEl);
+  const handleClick = (event) => {
+    setAnchorEl(event.currentTarget);
+  };
+  const handleClose = () => {
+    setAnchorEl(null);
+  };
 
-    return (
-        <div>
-            <Button
-                id="basic-button"
-                aria-controls={open ? 'basic-menu' : undefined}
-                aria-haspopup="true"
-                aria-expanded={open ? 'true' : undefined}
-                onClick={handleClick}
-            >
-                {props.name}
-            </Button>
-            <Menu
-                id="basic-menu"
-                anchorEl={anchorEl}
-                open={open}
-                onClose={handleClose}
-                MenuListProps={{
-                    'aria-labelledby': 'basic-button',
-                }}
-            >
-                <MenuItem component={Link} to={props.link1} onClick={handleClose}>
-                    {props.option1}
-                </MenuItem>
-                <MenuItem component={Link} to={props.link2} onClick={handleClose}>
-                    {props.option2}
-                </MenuItem>
-            </Menu>
-        </div>
-    );
-}
-
-export function ButtonLink(props) {
-    return (
-        <Link
-            component="button"
-            variant="body2"
-            to={props.to} // Use `to` instead of `href`
-            onClick={() => {
-                // Add any onClick functionality here if needed
-            }}
-        >
-            {props.children}
-        </Link>
-    );
+  return (
+    <div>
+      <Button
+        id="basic-button"
+        aria-controls={open ? "basic-menu" : undefined}
+        aria-haspopup="true"
+        aria-expanded={open ? "true" : undefined}
+        onClick={handleClick}
+      >
+        {props.name}
+      </Button>
+      <Menu
+        id="basic-menu"
+        anchorEl={anchorEl}
+        open={open}
+        onClose={handleClose}
+        MenuListProps={{
+          "aria-labelledby": "basic-button",
+        }}
+      >
+        <MenuItem component={Link} to={props.link1} onClick={handleClose}>
+          {props.option1}
+        </MenuItem>
+        <MenuItem component={Link} to={props.link2} onClick={handleClose}>
+          {props.option2}
+        </MenuItem>
+      </Menu>
+    </div>
+  );
 }
 
 export function ButtonAdmin(props) {
-    return (
-        <Button
-            type={props.type}
-            value={props.value}
-            style={{ marginTop: "25px", backgroundColor: "white", color: "black", borderColor: "black" }}
-            variant="outlined"
-        >
-            {props.label}
-        </Button>
-    );
+  return (
+    <Button
+      type={props.type}
+      value={props.value}
+      style={{
+        marginTop: "25px",
+        backgroundColor: "white",
+        color: "black",
+        borderColor: "black",
+      }}
+      variant="outlined"
+    >
+      {props.label}
+    </Button>
+  );
 }
+
+export function TextFieldsAdmin(props) {
+  return (
+    <TextField
+      id={props.id}
+      type={props.type}
+      label={props.label}
+      name={props.name}
+      variant="standard"
+      style={{ paddingBottom: "15px" }}
+      required
+    />
+  );
+}
+
+export function BasicSelect() {
+  const [estado, setEstado] = React.useState("");
+
+  const handleChange = (event) => {
+    setEstado(event.target.value);
+  };
+
+  return (
+    <Box sx={{ minWidth: 120 }}>
+      <FormControl fullWidth>
+        <InputLabel id="demo-simple-select-label">Estado</InputLabel>
+        <Select
+          labelId="demo-simple-select-label"
+          id="demo-simple-select"
+          value={estado}
+          label="Estado"
+          onChange={handleChange}
+        >
+          <MenuItem value={1}>Activo</MenuItem>
+          <MenuItem value={0}>Inactivo</MenuItem>
+        </Select>
+      </FormControl>
+    </Box>
+  );
+}
+
+export function BasicSelectTipo() {
+    const [estado, setEstado] = React.useState("");
+  
+    const handleChange = (event) => {
+      setEstado(event.target.value);
+    };
+  
+    return (
+      <Box sx={{ minWidth: 120 }}>
+        <FormControl fullWidth>
+          <InputLabel id="demo-simple-select-label">Tipo</InputLabel>
+          <Select
+            labelId="demo-simple-select-label"
+            id="demo-simple-select"
+            value={estado}
+            label="Estado"
+            onChange={handleChange}
+          >
+            <MenuItem value={1}>Corriente</MenuItem>
+            <MenuItem value={0}>Administrador</MenuItem>
+          </Select>
+        </FormControl>
+      </Box>
+    );
+  }
